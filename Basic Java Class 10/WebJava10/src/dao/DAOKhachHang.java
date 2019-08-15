42f08b9154fe7950e57e149eb17e745b916f7b52
Mai Van Ha <maivanha1610@gmail.com>
2019-08-15 19:34:46 +0700

    update code

--- Basic Java Class 10/WebJava10/src/dao/DAOKhachHang.java
+++ Basic Java Class 10/WebJava10/src/dao/DAOKhachHang.java
@@ -1,5 +1,6 @@
 package dao;
 
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -36,10 +37,58 @@ public class DAOKhachHang {
 		return khList;
 	}
 	
+	public void insertKhachHang(KhachHang kh) throws SQLException {
+		/*
+		sql = "INSERT INTO nhanvien (hoten, tendangnhap, matkhau, dienthoai) "
+				+ "VALUES (N'"+kh.getHoTen() + "', '"+ kh.getTenDangNhap() + 
+					"', SHA2('"+"', 512), '"+"');";
+		*/
+		String sql = "INSERT INTO khachhang (hoten, tendangnhap, matkhau, diachi, dienthoai) "
+				+ "VALUES (?, ?, SHA2(?, 512), ?, ?)";
+		PreparedStatement preSta = dbConnect.getConnection().prepareStatement(sql);
+		preSta.setString(1, kh.getHoTen());
+		preSta.setString(2, kh.getTenDangNhap());
+		preSta.setString(3, kh.getMatKhau());
+		preSta.setString(4, kh.getDiaChi());
+		preSta.setString(5, kh.getDienThoai());
+		
+		preSta.executeUpdate();
+	}
+	
+	public KhachHang login(String username, String password) throws SQLException {
+		KhachHang kh = null;
+		String sql = "SELECT * FROM khachhang WHERE "
+				+ "tendangnhap = ? AND matkhau = SHA2(?, 512)";
+		
+		PreparedStatement preSta = dbConnect.getConnection().prepareStatement(sql);
+		preSta.setString(1, username);
+		preSta.setString(2, password);
+		
+		ResultSet rs = preSta.executeQuery();
+		
+		while(rs.next()) {
+			kh = new KhachHang();
+			
+			kh.setMakh(rs.getInt("makh"));
+			kh.setHoTen(rs.getString("hoten"));
+			kh.setTenDangNhap(rs.getString("tendangnhap"));
+			kh.setMatKhau(rs.getString("matkhau"));
+			kh.setDiaChi(rs.getString("diachi"));
+			kh.setDienThoai(rs.getString("dienthoai"));
+		}
+		return kh;
+	}
+	
 	public static void main(String[] args) throws SQLException {
 		DAOKhachHang khDao = new DAOKhachHang();
-		for(KhachHang kh : khDao.getAllKhachHang()) {
+		/*KhachHang k = new KhachHang("Nguyen Van F", "fnv", "123456", "Ha NOi", "1235486971");
+		khDao.insertKhachHang(k);*/
+		
+		/*for(KhachHang kh : khDao.getAllKhachHang()) {
 			System.out.println(kh.getHoTen());
-		}
+		}*/
+		
+		KhachHang k = khDao.login("fnv", "123456");
+		System.out.println(k.getTenDangNhap());
 	}
 }
--- Basic Java Class 10/WebJava10/src/model/KhachHang.java
+++ Basic Java Class 10/WebJava10/src/model/KhachHang.java
@@ -7,6 +7,21 @@ public class KhachHang {
 	private String matKhau;
 	private String diaChi;
 	private String dienThoai;
+	
+	public KhachHang() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public KhachHang(String hoTen, String tenDangNhap, String matKhau, String diaChi, String dienThoai) {
+		this.hoTen = hoTen;
+		this.tenDangNhap = tenDangNhap;
+		this.matKhau = matKhau;
+		this.diaChi = diaChi;
+		this.dienThoai = dienThoai;
+	}
+
+
+
 	public int getMakh() {
 		return makh;
 	}
