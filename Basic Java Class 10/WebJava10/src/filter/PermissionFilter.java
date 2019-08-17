30b1dd85e25c4d5c6d13f9b4a45f9948e8b29add
Mai Van Ha <maivanha1610@gmail.com>
2019-08-17 12:16:09 +0700

    update filtering-code

---
+++ Basic Java Class 10/WebJava10/src/filter/PermissionFilter.java
@@ -0,0 +1,60 @@
+package filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.NhanVien;
+
+/**
+ * Servlet Filter implementation class PermissionFilter
+ */
+@WebFilter("/manage/*")
+public class PermissionFilter implements Filter {
+
+    /**
+     * Default constructor. 
+     */
+    public PermissionFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		HttpSession session = ((HttpServletRequest) request).getSession();
+		Object userObj = session.getAttribute("user");
+		if(userObj == null || (userObj instanceof NhanVien) == false) {
+			((HttpServletResponse) response).sendRedirect("../login.jsp");
+		} 		
+		
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
