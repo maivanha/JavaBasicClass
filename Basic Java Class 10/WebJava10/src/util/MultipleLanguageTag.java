b1ee0cd8d96989b318f50560f181d7113c55f330
maivanha <maivanha@maivanha-HP-EliteBook-8570w>
2019-08-29 19:05:49 +0700

    Update code

---
+++ Basic Java Class 10/WebJava10/src/util/MultipleLanguageTag.java
@@ -0,0 +1,14 @@
+package util;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class MultipleLanguageTag extends SimpleTagSupport {
+	@Override
+	public void doTag() throws JspException, IOException {
+		// TODO Auto-generated method stub
+		super.doTag();
+	}
+}
